e51f0d813746c2a3d82c7e6280f8a3076133da1d
Vlad Samoilov <vladsamoylovvlad@gmail.com>
2023-09-02 04:51:11 +0300

    Created solution of task g)

--- PROGRAMMING ON THE BASE OF LINEAR/MainSolution.cpp
+++ PROGRAMMING ON THE BASE OF LINEAR/MainSolution.cpp
@@ -2,12 +2,34 @@
 
 using namespace std;
 
+void NaturalNumberIsMultipleOfThreeOrFour(int n) {
+	if (n % 3 == 0 && n % 9 != 0) {
+		cout << "Natural Number - " << n << " Is TRUE! It Is Multiple Of 3 And NOT Multiple Of 9." << endl;
+	}
+	else if (n % 4 == 0 && n % 5 != 0 && n % 24 != 0) {
+		cout << "Natural Number - " << n << " Is TRUE! It Is Multiple Of 4 And NOT Multiple Of 5 And 24." << endl;
+	}
+	else {
+		if (n % 3 == 0) {
+			cout << "Natural Number - " << n << " Is FALSE! It Is  Multiple Of 9." << endl;
+		}
+		else if (n % 4 == 0) {
+			cout << "Natural Number - " << n << " Is FALSE! It Is Multiple Of 5 Or 24." << endl;
+		}
+		else {
+			cout << "Natural Number - " << n << " Is NOT Multiple Of 3 OR 4." << endl;
+		}
+	}
+}
+
 
 int main() {
+	int n;
+	cout << "Enter Natural Number: ";
+	cin >> n;
 
-	cout << "Hello world\n";
-
-
-
-	return 0;
+	NaturalNumberIsMultipleOfThreeOrFour(n);
+	
+	
+	return main();
 }
